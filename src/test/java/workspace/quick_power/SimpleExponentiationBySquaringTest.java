a77243e9f946655802cb1a4a0b11fdef8a6b4705
weitong <maojianwei.mjw@alibaba-inc.com>
2022-01-19 20:39:26 +0800

    【快速幂】

---
+++ src/test/java/workspace/quick_power/SimpleExponentiationBySquaringTest.java
@@ -0,0 +1,26 @@
+package workspace.quick_power;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 简单乘方的计算：
+ * 使用快速幂：迭代方程式
+ * 计算a^n
+ *      if n   == 0  -> 1;
+ *      if n%2 == 0 -> a^(n/2) * a^(n/2)
+ *      if n%2 == 1 -> a^(n-1) * a
+ *
+ * @author weitong
+ * @date 2022/01-17 7:58 下午
+ */
+public class SimpleExponentiationBySquaringTest {
+
+    @Test
+    public void test(){
+        System.out.println(SimpleExponentiationBySquaring.calculateContinuous(3,13));
+        System.out.println(SimpleExponentiationBySquaring.calculate(3,13));
+        System.out.println(SimpleExponentiationBySquaring.calculateNonRer(3,13));
+    }
+
+
+}
